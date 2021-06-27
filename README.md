667dc025e1c7ef85676b48fa48affa1b85f1c4a9
syd333 <sydmiranda@icloud.com>
2021-06-27 10:49:01 -0400

    notes on algo

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # frequency-counter-algo
+*this pattern uses objects or sets to collect values/frequencies of values
+*this can often avoid the need for nested loops or O(N^2) operations with arrays/strings
---
+++ frequencyCounterPattern.js
@@ -0,0 +1,13 @@
+// write a function called same, which accepts two arrays
+// the function should return true if every value in the array
+// has its corresponding value squared in the second array.
+// the frequency of values must be the same.
+
+same = () => {
+// two arrays as parameter 
+// if arr1 && arr2 are not the same length
+// return false 
+// if each index in arr2 is double each index in arr1
+// return true
+// otherwise false
+}
