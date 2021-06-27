ff57be8712119d061e824fee6535069656af571c
syd333 <sydmiranda@icloud.com>
2021-06-27 10:58:13 -0400

    solved

--- frequencyCounterPattern.js
+++ frequencyCounterPattern.js
@@ -3,11 +3,31 @@
 // has its corresponding value squared in the second array.
 // the frequency of values must be the same.
 
-same = () => {
-// two arrays as parameter 
-// if arr1 && arr2 are not the same length
+same = (arr1, arr2) => {
+  // two arrays as parameter
+  // if arr1 && arr2 are not the same length
+  // return false
+  // if each index in arr2 is double each index in arr1
+  // return true
+  // otherwise false
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  // iterate over arr1
+  for (let i = 0; i < arr1.length; i++) {
+      // what is indexOf one squared in index 2
+    let correctIndex = arr2.indexOf(arr1[i] ** 2);
+    // if correct index is not in array
+    // return false
+    if (correctIndex === -1) {
+      return false;
+    }
+    // removes squared match from array 
+    arr2.splice(correctIndex, 1);
+  }
+  return true;
+};
+
+//[1, 2, 3, 2], [9, 1, 4, 4]
+// line 19: if had [1, 5, 2, 3, 2] -- and if 25 is not in second arr
 // return false 
-// if each index in arr2 is double each index in arr1
-// return true
-// otherwise false
-}
